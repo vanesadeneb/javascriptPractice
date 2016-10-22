973411a6eae3ff4fdfbc7a37185fdf1054843c7d
Vanesa Villarreal <vanessa_diesver@hotmail.com>
2016-10-22 14:42:51 -0700

    Students exercise
    Programming with arrays and objects.

---
+++ students/js/students.js
@@ -0,0 +1,7 @@
+var students = [
+	{name: "Vanesa", track:"Full Stack Javascript", achievements:150, points:1000000},
+	{name: "Deneb", track:"FrontEnd Development", achievements:108, points:2000000},
+	{name: "Omar", track:"Full Stack Python", achievements:553, points:300000},
+	{name: "Francisco", track:"iOs Development", achievements:367, points:8000},
+	{name: "Maritza", track:"Web Design", achievements:103, points:6000}
+];
---
+++ students/js/students_report.js
@@ -0,0 +1,29 @@
+var message = '';
+var student;
+var search;
+
+function print(message) {
+	var outputDiv = document.getElementById("output");
+	outputDiv.innerHTML = message;
+}
+
+function getStudentReport(student){
+		var report = "<h2>Student: " + student.name + "</h2>";
+		report += "<p>Track: " + student.track + "</p>";
+		report += "<p>Achievements: " + student.achievements + "</p>";
+		report += "<p>Points: " + student.points + "</p>";
+		return report;
+}
+while(true){
+	search = prompt("Search student records: type a name : [Vanesa (or type 'quit' to end)]");
+	if (search === null || search.toLowerCase() === "quit"){
+		break;
+	}
+	for (var i = 0; i < students.length; i++){
+		student = students[i];
+		if (student.name === search) {
+			message = getStudentReport(student);
+			print(message);
+		}
+	}
+}
