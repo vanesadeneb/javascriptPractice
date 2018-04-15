a7ea14ace84fec0b9f61ca36f9eafdb1f3ea4eef
VanesaDeneb <vanessa_diesver@hotmail.com>
2018-04-15 13:55:49 -0700

    SumFibonacciNumber
    get the fibbonacci number and sum odd numbers less or equals than the given number.

---
+++ fcc/fibonacciNumbers.js
@@ -0,0 +1,17 @@
+function sumFibs(num) {
+  fibonnacciNumber = [1,1];
+  resultSumFibonacciNum = 0;
+  for(var i = 0; i < fibonnacciNumber.length; i++){
+    nextFibonacci = fibonnacciNumber[i] + fibonnacciNumber[i+1];
+    if(nextFibonacci <= num){
+      fibonnacciNumber.push(nextFibonacci);
+    }
+    if(fibonnacciNumber[i] % 2){
+      resultSumFibonacciNum += fibonnacciNumber[i];
+    }
+  
+  }
+  return resultSumFibonacciNum;
+}
+
+sumFibs(4);
